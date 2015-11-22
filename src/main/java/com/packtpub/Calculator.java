068d3e366fba1fd72547e0ddc42fdb26a33311e8
vumvo <minhvu.2711@gmail.com>
2015-11-22 17:21:42 +0700

    Fix unit test folder

--- src/main/java/com/packtpub/Calculator.java
+++ src/main/java/com/packtpub/Calculator.java
@@ -2,6 +2,7 @@
  * Created by vumvo on 11/22/2015.
  */
 package com.packtpub;
+
 public class Calculator {
     public int sum(int x, int y) {
         return x + y;
---
+++ src/test/java/com/packtpub/CalculatorTest.java
@@ -0,0 +1,24 @@
+package com.packtpub;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * Created by vumvo on 11/22/2015.
+ */
+public class CalculatorTest extends TestCase {
+
+        public void testSum() throws Exception {
+            Calculator calculator = new Calculator();
+            int sum = calculator.sum(1, 2);
+            Assert.assertEquals(3, sum);
+        }
+
+        public void testFail() throws Exception {
+            Assert.fail("Some bad test has failed");
+        }
+
+        public void testPass() throws Exception {
+            Assert.assertEquals(3, 3);
+        }
+}
