8fa5ef382c4a5d09f25301487d9a5bd71802b73e
vumvo <minhvu.2711@gmail.com>
2015-11-22 17:02:45 +0700

    Add Fail Test

--- src/test/com/packtpub/CalculatorTest.java
+++ src/test/com/packtpub/CalculatorTest.java
@@ -3,6 +3,9 @@ package com.packtpub;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import java.lang.AssertionError;
+import java.lang.Exception;
+
 /**
  * Created by vumvo on 11/22/2015.
  */
@@ -12,5 +15,9 @@ public class CalculatorTest extends TestCase {
         Calculator calculator = new Calculator();
         int sum = calculator.sum(1, 2);
         Assert.assertEquals(3, sum);
+    }
+
+    public void testFail() throws Exception {
+        Assert.fail("Some bad test has failed");
     }
 }
